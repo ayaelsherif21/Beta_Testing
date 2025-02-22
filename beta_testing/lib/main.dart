9a59d5253e6e6178c336258a0be1dda72ab84129
Aya Elsherif <96848805+ayaelsherif21@users.noreply.github.com>
2025-02-22 23:42:24 +0200

    Beta Testing

---
+++ beta_testing/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Beta Test App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Beta Testing App'),
+        ),
+        body: Center(
+          child: Text('Welcome to Beta Testing!'),
+        ),
+      ),
+    );
+  }
+}
